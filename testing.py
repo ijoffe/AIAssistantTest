aef314f4aff275e24c68922ffa9905ee2123fda4
Isaac Joffe <90410161+ijoffe@users.noreply.github.com>
2024-07-29 13:51:31 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -35,7 +35,7 @@ def objectdetection_model(video_stream, od_model):
     od_model.predict(
         source=video_stream,
         show=True,              # open window with live labels
-        vid_stride=25,           # skip every other frame for speed
+        vid_stride=2,           # skip every other frame for speed
         verbose=False,          # don't print outputs
         device='0',           # large language model gets to use gpu
     )
@@ -58,7 +58,7 @@ def largelanguage_model(video_stream, ll_model, ll_processor):
             cap.set(cv2.CAP_PROP_POS_FRAMES, (time.time()-start_time)*fps-1)
             res, frame = cap.read()
             invoke_url = "https://ai.api.nvidia.com/v1/vlm/community/llava16-34b"
-            stream = True
+            stream = False
             _, buffer = cv2.imencode('.png', frame)
             image_b64 = base64.b64encode(buffer).decode()
             headers = {
@@ -80,12 +80,7 @@ def largelanguage_model(video_stream, ll_model, ll_processor):
                 "stream": stream
             }
             response = requests.post(invoke_url, headers=headers, json=payload)
-            message = response.json().get('choices', [{}])[0].get('text', '')
-            # prompt = "USER: You are a safety inspector tasked with analyzing a construction site for potential safety hazards. Review each image provided, identify and describe all hazards, explain why each is a hazard, and suggest ways to eliminate or reduce the danger of each. The goal is to make the construction site safer and prevent incidents.\nRespond clearly, concisely, and professionally only in the following format:\n**Hazard**: [Description of the hazard]\n**Explanation**: [Brief explanation of why it is a hazard]\n**Suggestion**: [Suggestion to eliminate or reduce the danger]\nDo not repeat previously reported hazards. If no hazards exist, respond only with \"**NONE**\".\n\nUSER: <image>\n\nASSISTANT: "
-            # inputs = ll_processor(prompt, images=[frame], padding=True, return_tensors="pt").to("cuda")
-            # output = ll_model.generate(**inputs, max_new_tokens=512)
-            # generated_text = ll_processor.batch_decode(output, skip_special_tokens=True)
-            # message = generated_text[0].split("ASSISTANT: \n")[1].strip()
+            message = response.json().get('choices', [{}])[0].get('message', '').get('content', '')
             print(f"New message posted:\n\"{message}\"")
             for client in clients:
                 await client.send(message)
